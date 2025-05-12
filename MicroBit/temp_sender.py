9df9ab31f6c2d9def9de4cc6827eb50aee91663d
Oguz Alp <og.alpalp+GitHub@gmail.com>
2025-05-12 16:53:22 +0100

    Update temp_sender.py
    temperature sensor was initialised outside of the loop which meant the reading was not continuous but rather on setup. So it sent the same temperature reading over and over. The problem was fixed simply by moving it into the loop. Rookie mistake ahh

--- MicroBit/temp_sender.py
+++ MicroBit/temp_sender.py
@@ -1,8 +1,7 @@
 #Python script for Microsoft MakeCode (see https://makecode.microbit.org/)
 
-temp = input.temperature()
-
 def on_forever():
+    temp = input.temperature()
     buf = bytearray(1)
     buf.set_number(NumberFormat.UINT8_LE, 0, temp)
     pins.i2c_write_buffer(13, buf, False)
