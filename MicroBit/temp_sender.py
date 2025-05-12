7552fb725ebf181f83ab30d9905f747e6b8a38ac
Oguz Alp <og.alpalp+GitHub@gmail.com>
2025-05-12 20:23:02 +0100

    Update temp_sender.py
    some stylistic changes due to the last update on temp_receiver_display.ino

--- MicroBit/temp_sender.py
+++ MicroBit/temp_sender.py
@@ -1,9 +1,12 @@
 #Python script for Microsoft MakeCode (see https://makecode.microbit.org/)
 
+IC = 13; #I2C slave address
+READING_TIME = 1000 #time between each reading in ms
+
 def on_forever():
     temp = input.temperature()
     buf = bytearray(1)
     buf.set_number(NumberFormat.UINT8_LE, 0, temp)
-    pins.i2c_write_buffer(13, buf, False)
-    basic.pause(1000)
+    pins.i2c_write_buffer(IC, buf, False)
+    basic.pause(READING_TIME)
 basic.forever(on_forever)
