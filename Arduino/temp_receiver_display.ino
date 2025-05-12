ecf9371a00123d490de075d507451f44dfc3a1ff
Oguz Alp <og.alpalp+GitHub@gmail.com>
2025-05-12 20:19:23 +0100

    Update temp_receiver_display.ino
    fixed the the problem of the display not flushing after each loop. This was the cause of numbers stacking up in the case of temperature change. It was left unnoticed since the temperature reading was static anyways before the last update on temp_sender.py.

--- Arduino/temp_receiver_display.ino
+++ Arduino/temp_receiver_display.ino
@@ -12,11 +12,13 @@ ArduinoLEDMatrix matrix;
 const int SENTINEL = -1; //choose a value that you never expect to recieve from the microbit
 const int SERIAL_CHANNEL = 115200; //display the serial monitor on this channel
 const int IC = 13; //I2C slave address
+const int READING_TIME = 1000; //time between each reading in ms. Needs to match with the microbit code
 
 volatile int received_value = SENTINEL;
 volatile bool new_data_status = false;
 
 void youve_got_mail(int doesnt_matter); //I2C receive callback: triggered automatically when Microbit sends data
+void reset_frame(); //resets the frame to default position:
 
 uint8_t frame[8][12] = { //Default frame always showing the degrees Celsius symbol
    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -43,8 +45,11 @@ void loop() {
     }
     else { //everything's fine yippie:
       Serial.println(received_value);
+
       drawNumber(received_value, frame);
       matrix.renderBitmap(frame, 8, 12);
+      delay(READING_TIME);
+      reset_frame(); //resetting for the next loop
     }
     new_data_status = false; //resetting for the next loop
   }
@@ -54,5 +59,14 @@ void youve_got_mail(int doesnt_matter) {
   if (Wire.available()) {
     received_value = Wire.read();
     new_data_status = true;
+  }
+}
+
+void reset_frame() {
+  matrix.renderBitmap(frame, 8, 12);
+  for (int x = 0; x <= 7; x++) { //up to the Celsius symbol
+    for (int y = 0; y <= 7; y++) {
+      frame[y][x] = 0;
+    }
   }
 }
